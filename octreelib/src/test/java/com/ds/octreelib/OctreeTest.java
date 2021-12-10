efc08bff218326531ca074ab4f53836753fdaada
MarcoCiaramella <marcociaramella87@gmail.com>
2021-12-10 08:17:43 +0100

    wip

---
+++ octreelib/src/test/java/com/ds/octreelib/OctreeTest.java
@@ -0,0 +1,13 @@
+package com.ds.octreelib;
+
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Test;
+
+public class OctreeTest {
+
+    @Test
+    public void LolNewOctree_isCorrect() {
+        assertEquals(4, 2 + 2);
+    }
+}
